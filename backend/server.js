789680bb7b9ea664a6cd8d55e83874fd1b90a3a4
hoang tuan minh <hoangtuanminh25@gmail.com>
2025-02-25 22:36:51 +0700

    Simple project with MySQL

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,56 @@ const cors = require("cors");
 const mysql = require("mysql");
 
 const app = express();
+app.use(express.json());
 
 app.use(cors());
 
-mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  databse: "crud",
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "crud",
+	port: "3307",
 });
 
 app.get("/", (req, res) => {
-  res.json("Backend connected");
+	const sql = "SELECT * FROM student";
+	db.query(sql, (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
+});
+
+app.post("/create", (req, res) => {
+	const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?)";
+	const value = [req.body.name, req.body.email];
+	db.query(sql, [value], (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
+});
+
+app.put("/update/:id", (req, res) => {
+	const sql = "UPDATE student set `Name` = ?, `Email` = ? WHERE ID = ?";
+	const value = [req.body.name, req.body.email];
+	const id = req.params.id;
+
+	db.query(sql, [...value, id], (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
+});
+
+app.delete("/student/:id", (req, res) => {
+	const sql = "DELETE FROM student where ID = ?";
+	const id = req.params.id;
+
+	db.query(sql, [id], (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
 });
 
 app.listen(8081, () => {
-  console.log("Init project");
+	console.log("Init project");
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Student from "./Student";
+import CreateStudent from "./CreateStudent";
+import UpdateStudent from "./UpdateStudent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Student />}></Route>
+					<Route path="/create" element={<CreateStudent />}></Route>
+					<Route
+						path="/update/:id"
+						element={<UpdateStudent />}
+					></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
